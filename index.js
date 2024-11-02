df19ee61e4d04b9eb57bf2cc64843673364ae210
lionel staricoff <staricofflionel@hotmail.com>
2024-11-02 16:39:53 -0500

    sacando errores en el html

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 
 // import cors from "cors";
+// import appRoutes from "./js/routers/appRoutes.js";
 
+// import path from 'path';
 
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ReactDOMServer from 'react-dom/server';
-import {App} from'./componentes/pages_react/app.js';
 
 
 import express from 'express';
+//import path from'path';
 
-
-
-const html = ReactDOMServer.renderToString(App);
 
 const app = express();
 app.disable('x-powered-by')
@@ -21,18 +16,32 @@ const port = 3001;
 
 
 
+//const __dirname = '';
 
-
+// Middleware para servir archivos estáticos
+//app.use(express.static(path.join(__dirname, 'public')));
 
 //Gladys
-app.use(express.static(html));
-//app.use('/home',express.static('componentes'));
-
+app.use('/home',express.static('componentes'));
 //Luma
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 
 
+
+/* Ruta para el index
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});*/
+
+/*app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});*/
+
+/*app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'componentes', 'indexGLA.html'));
+
+});*/
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
